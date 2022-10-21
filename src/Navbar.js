f57e2fe59b0a7792ab4a8074388781d05ea3e1db
burnbadd <shw43@cam.ac.uk>
2022-10-21 16:08:08 +0100

    Added Halloween cat!

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,16 +2,19 @@ import {Link, useMatch, useResolvedPath} from "react-router-dom"
 export default function Navbar(){
   return (
   <nav className="nav">
-    <Link to="/" className="site-title">CamPhys</Link>
     <ul>
-        <CustomLink to="/TSP">TSP</CustomLink>
-        <CustomLink to="/REL">REL</CustomLink>
-        <CustomLink to="/AQP">AQP</CustomLink>
-        <CustomLink to="/OE">OE</CustomLink>
-        <CustomLink to="/AFD">AFD</CustomLink>
-        <CustomLink to="/PNP">PNP</CustomLink>
-        <CustomLink to="/QCM">QCM</CustomLink>
-        <CustomLink to="/SCM">SCM</CustomLink>
+      <Link to="/" className="site-title">CamPhys</Link>
+      <img src="cat_ghost_halloween_22016.png" title="Happy Halloween!"></img>
+    </ul>
+    <ul>
+      <CustomLink to="/TSP">TSP</CustomLink>
+      <CustomLink to="/REL">REL</CustomLink>
+      <CustomLink to="/AQP">AQP</CustomLink>
+      <CustomLink to="/OE">OE</CustomLink>
+      <CustomLink to="/AFD">AFD</CustomLink>
+      <CustomLink to="/PNP">PNP</CustomLink>
+      <CustomLink to="/QCM">QCM</CustomLink>
+      <CustomLink to="/SCM">SCM</CustomLink>
     </ul>
   </nav>
   )
